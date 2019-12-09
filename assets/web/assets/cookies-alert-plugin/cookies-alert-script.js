e392b29c690536d35780865144a91d1d68c95e19
Vikram Chalana <vikram@chalana.com>
2019-12-09 13:53:14 -0800

    Privacy Policy Related Changes

--- assets/web/assets/cookies-alert-plugin/cookies-alert-script.js
+++ assets/web/assets/cookies-alert-plugin/cookies-alert-script.js
@@ -1,4 +1,3 @@
-!function(){try{document.getElementsByClassName("engine")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.className="engine";a.innerHTML='<a href="https://mobirise.ws">Mobirise Website Builder</a> v4.11.6';document.body.insertBefore(a,document.body.childNodes[0])}}();
 jQuery(function () {
     var $ = jQuery,
         tmpInput = $('input[name=cookieData]');
@@ -24,3 +23,4 @@ jQuery(function () {
 
     tmpInput.remove();
 });
+!function(){try{document.getElementsByClassName("engine")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.className="engine";a.innerHTML='<a href="https://mobirise.ws">Mobirise Website Builder</a> v4.11.6';document.body.insertBefore(a,document.body.childNodes[0])}}();
